6b4ed9d27a7995f755da0adc342a9103e8db4189
soliman <abdelrahman.lifestyle@gmail.com>
2018-02-19 22:26:33 +0200

    design upates

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -8,26 +8,24 @@ import { BackendServiceService } from '../../service/backend-service.service';
 })
 export class HomeComponent implements OnInit {
 
+  doubleWidthIndex:any = [0,null,null,null,null,null,6];
   rssData = [];
   showedRssData:any = [];
-  // restOfRssData:any = [];
   nummberOfClicks:number = 1;
 
   constructor(public serviceData: BackendServiceService) {
     this.serviceData.getRssData().subscribe(rssData => {
     this.rssData = rssData.items;
-      console.log(this.rssData);
       this.showedRssData = this.rssData.slice(0,7);
-      // this.restOfRssData = this.rssData.slice(7,this.rssData.length);
     });
+    
   }
 
   ngOnInit() {
   }
 
   onViewMore(){
-    this.nummberOfClicks = 2;
+    this.nummberOfClicks++;
     this.showedRssData = this.rssData.slice(0 , 7*this.nummberOfClicks);
   }
-
 }
