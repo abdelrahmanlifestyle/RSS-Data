caca8569aa158fea96eed025902977935e719e13
soliman <abdelrahman.lifestyle@gmail.com>
2018-02-20 19:22:35 +0200

    style and double width index

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -6,26 +6,33 @@ import { BackendServiceService } from '../../service/backend-service.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-  doubleWidthIndex:any = [0,null,null,null,null,null,6];
-  rssData = [];
+  rssData:any = [];
   showedRssData:any = [];
   nummberOfClicks:number = 1;
+  doublicatedWidthIndex:[number] = [0,6];
 
   constructor(public serviceData: BackendServiceService) {
     this.serviceData.getRssData().subscribe(rssData => {
     this.rssData = rssData.items;
       this.showedRssData = this.rssData.slice(0,7);
     });
-    
   }
 
   ngOnInit() {
   }
 
   onViewMore(){
+    this.doublicatedWidthIndex.push(7*this.nummberOfClicks);
     this.nummberOfClicks++;
+    this.doublicatedWidthIndex.push(7*this.nummberOfClicks-1);
     this.showedRssData = this.rssData.slice(0 , 7*this.nummberOfClicks);
   }
+
+  doublicatedIndexChecker(index){
+    return (this.doublicatedWidthIndex.indexOf(index)>=0);
+  }
+
 }
